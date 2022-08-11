feea6dce903ac79b48ebeedfe81a19031ea868a8
Dan-Abr <da208@student.london.ac.uk>
2022-08-11 16:05:44 +0300

    feat: allow users to access specific e2e-test via API endpoint with CRUD.

--- core_app/api.py
+++ core_app/api.py
@@ -29,6 +29,34 @@ class E2ETestParamsList(mixins.ListModelMixin,
         return self.create(request, *args, **kwargs)
 
 
+class E2ETestParamsID(mixins.CreateModelMixin,
+                      mixins.RetrieveModelMixin,
+                      mixins.UpdateModelMixin,
+                      mixins.DestroyModelMixin,
+                      generics.GenericAPIView):
+    """ A specific end-to-end test, which belong to the user,
+        based on the end-to-end ID.
+        Allow to update, delete and retrieve the end-to-end test result.
+    """
+    queryset = E2ETestParamsModel.objects.all()
+    serializer_class = E2ETestParamsSerializer
+    # Only logged-in users can access this API endpoint.
+    # Users can only access their own end-to-end tests.
+    permission_classes = [IsUser]
+
+    def post(self, request, *args, **kwargs):
+        return self.create(request, *args, **kwargs)
+
+    def get(self, request, *args, **kwargs):
+        return self.retrieve(request, *args, **kwargs)
+
+    def put(self, request, *args, **kwargs):
+        return self.update(request, *args, **kwargs)
+
+    def delete(self, request, *args, **kwargs):
+        return self.destroy(request, *args, **kwargs)
+
+
 class E2ETestResultsList(mixins.ListModelMixin,
                          generics.GenericAPIView):
     """ List all existing end-to-end test results, which belong to the user.
@@ -56,7 +84,7 @@ class E2ETestResultID(mixins.CreateModelMixin,
                       mixins.DestroyModelMixin,
                       generics.GenericAPIView):
     """ A specific end-to-end test result, which belong to the user,
-        based on the end-to-end ID.
+        based on the end-to-end result ID.
         Allow to update, delete and retrieve the end-to-end test result.
     """
     queryset = E2ETestResultsModel.objects.all()
