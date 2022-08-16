d7fce951538dc9b307ef29a1b32520a1803157fa
Dan-Abr <da208@student.london.ac.uk>
2022-08-16 14:49:26 +0300

    fix: django-celery-beat could not call tasks due to variable typo.

--- core_app/tasks.py
+++ core_app/tasks.py
@@ -11,11 +11,11 @@ app = Celery('tasks', broker='redis://localhost/', backend='redis://localhost/')
 
 
 @shared_task
-def call_crawl_website(user_pk=None, test_pk=None, url="", tasks=None):
-    if user_pk == None or test_pk == None or tasks == None:
+def call_crawl_website(user_pk=None, e2e_test_pk=None, url="", tasks=None):
+    if user_pk == None or e2e_test_pk == None or tasks == None:
         # Must belong to an active user.
         # Must belong to existing e2e-test.
         # Must have tasks to do in the website.
         return
-    crawl_website(user_pk, test_pk, url, tasks)
+    crawl_website(user_pk, e2e_test_pk, url, tasks)
     return
