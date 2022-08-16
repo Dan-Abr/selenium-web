1dfa2daeedaf72c8fb98529108bbab3bc335e547
Dan-Abr <da208@student.london.ac.uk>
2022-08-17 00:15:16 +0300

    feat: raise exceptions when calling a crawler with null data.

--- core_app/tasks.py
+++ core_app/tasks.py
@@ -10,12 +10,19 @@ app = Celery('tasks', broker='redis://localhost/', backend='redis://localhost/')
 
 @shared_task
 def call_crawl_website(user_pk=None, e2e_test_params_pk=None, url=None, tasks=None):
-    if user_pk == None or e2e_test_params_pk == None or url == None or tasks == None:
+    if user_pk == None:
         # Must belong to an active user.
+        raise TypeError('user_pk cannot be null')
+    elif e2e_test_params_pk == None:
         # Must belong to existing e2e-test.
-        # Must have tasks to do in the website.
-
-        # Raise error
-        return
+        raise TypeError('e2e_test_params_pk cannot be null')
+    elif url == None:
+        # Must have a valid URL.
+        raise TypeError('URL cannot be null')
+    elif tasks == None:
+        #  Must have tasks to do in the website.
+        raise TypeError('tasks cannot be null')
+    
+    # Everything is fine, call the crawler.
     crawl_website(user_pk, e2e_test_params_pk, url, tasks)
     return
--- core_app/tests/test_crawlers.py
+++ core_app/tests/test_crawlers.py
@@ -1,7 +1,6 @@
 # Django
-from django.test import TestCase
+from django.test import TestCase, TransactionTestCase
 from django.contrib.auth.models import User
-from django.urls import reverse
 
 # local Django
 from ..crawlers import crawl_website
@@ -15,7 +14,7 @@ from ..models import E2ETestResultsModel
 # Since it is aimed to be implemented on a server or self-hosted,
 # such as raspberry pi, the application was tested on Linux (Debian, Ubuntu).
 # I did not test the application on Windows or MacOS.
-class TestCrawler(TestCase):
+class TestCrawlerTransaction(TransactionTestCase):
     user_dummy_credentials_1 = None
 
     def setUp(self):
@@ -27,7 +26,6 @@ class TestCrawler(TestCase):
 
         # Create Django's default user 
         User.objects.create_user(**self.user_dummy_credentials_1)
-
 
     def tearDown(self):
         User.objects.all().delete()
@@ -55,10 +53,91 @@ class TestCrawler(TestCase):
             crawl_website(user_pk, e2e_test_params_pk, url, tasks)
             # Assert: the test shouldn't pass since user_pk 2 doesn't exist.
             self.assertTrue(False)
-        except Exception as e:
+        except Exception:
+            # Assert: the test should raise exception.
+            self.assertTrue(True)
+    
+    def test_crawl_website_will_raise_exception_if_user_pk_is_null(self):
+        # Arrange
+        user_pk = None
+        e2e_test_params_pk = 0
+        url = 'https://www.google.com'
+        tasks = [{'1': 1}]
+        # Act
+        try:
+            crawl_website(user_pk, e2e_test_params_pk, url, tasks)
+            # Assert: the test shouldn't pass since user_pk is None.
+            self.assertTrue(False)
+        except Exception:
             # Assert: the test should raise exception.
             self.assertTrue(True)
 
+    def test_crawl_website_will_raise_exception_if_e2e_test_params_pk_is_null(self):
+        # Arrange
+        user_pk = 1
+        e2e_test_params_pk = None
+        url = 'https://www.google.com'
+        tasks = [{'1': 1}]
+        # Act
+        try:
+            crawl_website(user_pk, e2e_test_params_pk, url, tasks)
+            # Assert: the test shouldn't pass since e2e_test_params_pk is None.
+            self.assertTrue(False)
+        except Exception:
+            # Assert: the test should raise exception.
+            self.assertTrue(True)
+
+    def test_crawl_website_will_raise_exception_if_url_is_null(self):
+        # Arrange
+        user_pk = 1
+        e2e_test_params_pk = 0
+        url = None
+        tasks = [{'1': 1}]
+        # Act
+        try:
+            crawl_website(user_pk, e2e_test_params_pk, url, tasks)
+            # Assert: the test shouldn't pass since url is None.
+            self.assertTrue(False)
+        except Exception:
+            # Assert: the test should raise exception.
+            self.assertTrue(True)
+        
+    def test_crawl_website_will_raise_exception_if_tasks_is_null(self):
+        # Arrange
+        user_pk = 1
+        e2e_test_params_pk = 0
+        url = 'https://www.google.com'
+        tasks = None
+        # Act
+        try:
+            crawl_website(user_pk, e2e_test_params_pk, url, tasks)
+            # Assert: the test shouldn't pass since tasks is None.
+            self.assertTrue(False)
+        except Exception:
+            # Assert: the test should raise exception.
+            self.assertTrue(True)
+
+
+class TestCrawler(TestCase):
+    user_dummy_credentials_1 = None
+
+    def setUp(self):
+        self.user_dummy_credentials_1 = {
+            'username': 'test_user1',
+            'email': 'test1@tests.com',
+            'password': 'Test12321',
+        }
+
+        # Create Django's default user 
+        User.objects.create_user(**self.user_dummy_credentials_1)
+
+    def tearDown(self):
+        User.objects.all().delete()
+
+    
+    # Using TestCase instead of TransactionTestCase because the latter has
+    # different mechanism to handle ORM database.
+    # Resource: https://stackoverflow.com/questions/43978468/django-test-transactionmanagementerror-you-cant-execute-queries-until-the-end
     def test_crawl_website_will_return_list_of_errors_if_invalid_XPath(self):
         # Arrange
         user_pk = 1
@@ -70,5 +149,3 @@ class TestCrawler(TestCase):
         errors = E2ETestResultsModel.objects.filter(user=user_pk).values_list('error_list', flat=True)
         # Assert
         self.assertTrue(len(errors) > 0)
-
-    
